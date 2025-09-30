675315b6171ca2748e8ce526eab16ae443797b36
frozenfirebomb <bartvrugt@hotmail.com>
2025-09-30 07:13:09 -0400

    First commit of aiagent course

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+from google import genai
+
+client = genai.Client(api_key=api_key)
+
+
+
+def main():
+    print("Hello from aiagent!")
+
+
+if __name__ == "__main__":
+    main()
