02c9f1c1090af7d2228e4706161416bc12777400
frozenfirebomb <bartvrugt@hotmail.com>
2025-10-01 07:19:43 -0400

    aiagent CH1 L4

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
 
 
 def main():
+
+    args= sys.argv[1:]
+
+    if not args:
+        print("AI code assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "how do I build a calculator app?"')
+        sys.exit(1)
+
+    user_prompt = " ".join(args)
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        model='gemini-2.0-flash-001', contents= user_prompt
     )
     print("Prompt tokens:", response.usage_metadata.prompt_token_count)
     print("Response tokens:", response.usage_metadata.candidates_token_count)
